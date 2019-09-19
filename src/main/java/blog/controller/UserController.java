fac27a6d409871af1958daf5f07a59bcf4cea44b
nikitapatle <nikita.patle@gmail.com>
2019-09-19 16:46:46 +0530

    new

--- src/main/java/blog/controller/UserController.java
+++ src/main/java/blog/controller/UserController.java
@@ -1,6 +1,11 @@
 package blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController
 {
+	@RequestMapping
 public void m1(){}
 }
